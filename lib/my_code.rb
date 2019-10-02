1648580416a1a06cd666236843d26b0d4c6eb6e4
Daniel Wilder <dauncy@gmail.com>
2019-10-02 19:52:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-# My Code here....
+
 def map_to_negativize (source_array)
   i = 0 
   new_array=[]
@@ -33,3 +33,21 @@ def map_to_square(source_array)
   end 
   new_array
 end 
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end
