5faff93b1d420af39cdfbc09b96ea891ca023b68
Joseph  Louie <joseph161216@gmail.com>
2019-10-25 05:08:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,78 @@
 # My Code here....
+def map_to_negativize(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length do 
+    new_arr << arr[i] * -1
+    i += 1
+  end 
+  new_arr
+end 
+
+def map_to_no_change(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length do 
+    new_arr << arr[i]
+    i += 1
+  end 
+  new_arr
+end 
+
+def map_to_double(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length do 
+    new_arr << arr[i] + arr[i]
+    i += 1
+  end 
+  new_arr
+end 
+
+def map_to_square(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length do 
+    new_arr << arr[i] * arr[i]
+    i += 1
+  end 
+  new_arr
+end 
+
+def reduce_to_total(arr, starting_point = 0)
+  i = 0 
+  while i < arr.length do 
+    starting_point += arr[i]
+    i += 1
+  end 
+  starting_point
+end 
+
+def reduce_to_all_true(arr)
+  i = 0 
+  while i < arr.length do 
+    if arr[i] == false
+      return false
+    end 
+    i += 1 
+  end 
+  true
+end 
+
+def reduce_to_any_true(arr)
+  i = 0 
+  while i < arr.length do 
+    if arr[i] == true
+      return true
+    end 
+    i += 1
+  end 
+  false
+end 
+
+
+
+
+
+
+
